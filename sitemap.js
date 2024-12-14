f204e45667e5ac8af9c7dcaed68c5450ef747b55
DarKsandr <gdarksandr@gmail.com>
2024-12-14 13:02:26 +0400

    add sitemap

---
+++ sitemap.js
@@ -0,0 +1,18 @@
+import {createWriteStream} from 'fs';
+import { SitemapStream } from 'sitemap';
+import 'dotenv/config'
+import {routes} from './src/routes.js';
+
+const hostname = process.env.VITE_HOSTNAME;
+
+// Creates a sitemap object given the input configuration with URLs
+const sitemap = new SitemapStream({ hostname });
+
+const writeStream = createWriteStream(process.env.VITE_PUBLIC + '/sitemap.xml');
+sitemap.pipe(writeStream);
+
+routes.forEach(route => {
+    sitemap.write(hostname + route.path);
+});
+
+sitemap.end();
