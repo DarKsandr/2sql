569a22c45a44e6cab5fd40589c991c7a35f6a00f
DarKsandr <gdarksandr@gmail.com>
2024-12-14 13:40:21 +0400

    fix robots

--- robots.js
+++ robots.js
@@ -5,5 +5,5 @@ import { EOL } from "node:os";
 const writeStream = createWriteStream(process.env.VITE_ROBOTS);
 writeStream.write('User-agent: *' + EOL);
 writeStream.write('Allow: /' + EOL + EOL);
-writeStream.write('Sitemap: ' + process.env.VITE_SITEMAP);
+writeStream.write('Sitemap: ' + process.env.VITE_SITEMAP_URL);
 writeStream.end();
